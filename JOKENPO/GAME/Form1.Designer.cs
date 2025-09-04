a17587e0b122da3c1b66693c4ebb5b48b109c41c
João Henrique <141533815+JoaoHenriqueHSR@users.noreply.github.com>
2025-09-04 02:58:39 -0300

    Initial Commit

---
+++ JOKENPO/GAME/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace GAME
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGame));
+            this.gpEscolhas = new System.Windows.Forms.GroupBox();
+            this.rbPedra = new System.Windows.Forms.RadioButton();
+            this.rbTesoura = new System.Windows.Forms.RadioButton();
+            this.rbPapel = new System.Windows.Forms.RadioButton();
+            this.rbDefault = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbLegenda = new System.Windows.Forms.Label();
+            this.lbPontuacao = new System.Windows.Forms.Label();
+            this.lbPonto = new System.Windows.Forms.Label();
+            this.picEscolha = new System.Windows.Forms.PictureBox();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.gpEscolhas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picEscolha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpEscolhas
+            // 
+            this.gpEscolhas.Controls.Add(this.rbPedra);
+            this.gpEscolhas.Controls.Add(this.rbTesoura);
+            this.gpEscolhas.Controls.Add(this.rbPapel);
+            this.gpEscolhas.Controls.Add(this.rbDefault);
+            this.gpEscolhas.Location = new System.Drawing.Point(89, 312);
+            this.gpEscolhas.Name = "gpEscolhas";
+            this.gpEscolhas.Size = new System.Drawing.Size(600, 100);
+            this.gpEscolhas.TabIndex = 0;
+            this.gpEscolhas.TabStop = false;
+            // 
+            // rbPedra
+            // 
+            this.rbPedra.BackColor = System.Drawing.Color.Transparent;
+            this.rbPedra.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbPedra.BackgroundImage")));
+            this.rbPedra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbPedra.Location = new System.Drawing.Point(71, 19);
+            this.rbPedra.Name = "rbPedra";
+            this.rbPedra.Size = new System.Drawing.Size(97, 70);
+            this.rbPedra.TabIndex = 3;
+            this.rbPedra.TabStop = true;
+            this.rbPedra.Text = "Pedra";
+            this.rbPedra.UseVisualStyleBackColor = false;
+            // 
+            // rbTesoura
+            // 
+            this.rbTesoura.BackColor = System.Drawing.Color.Transparent;
+            this.rbTesoura.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbTesoura.BackgroundImage")));
+            this.rbTesoura.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbTesoura.Location = new System.Drawing.Point(474, 19);
+            this.rbTesoura.Name = "rbTesoura";
+            this.rbTesoura.Size = new System.Drawing.Size(97, 70);
+            this.rbTesoura.TabIndex = 2;
+            this.rbTesoura.TabStop = true;
+            this.rbTesoura.Text = "Tesoura";
+            this.rbTesoura.UseVisualStyleBackColor = false;
+            // 
+            // rbPapel
+            // 
+            this.rbPapel.BackColor = System.Drawing.Color.Transparent;
+            this.rbPapel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbPapel.BackgroundImage")));
+            this.rbPapel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbPapel.Location = new System.Drawing.Point(252, 19);
+            this.rbPapel.Name = "rbPapel";
+            this.rbPapel.Size = new System.Drawing.Size(97, 70);
+            this.rbPapel.TabIndex = 1;
+            this.rbPapel.TabStop = true;
+            this.rbPapel.Text = "Papel";
+            this.rbPapel.UseVisualStyleBackColor = false;
+            // 
+            // rbDefault
+            // 
+            this.rbDefault.AutoSize = true;
+            this.rbDefault.Location = new System.Drawing.Point(6, 77);
+            this.rbDefault.Name = "rbDefault";
+            this.rbDefault.Size = new System.Drawing.Size(59, 17);
+            this.rbDefault.TabIndex = 0;
+            this.rbDefault.TabStop = true;
+            this.rbDefault.Text = "Default";
+            this.rbDefault.UseVisualStyleBackColor = true;
+            this.rbDefault.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbLegenda
+            // 
+            this.lbLegenda.AutoSize = true;
+            this.lbLegenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLegenda.Location = new System.Drawing.Point(340, 26);
+            this.lbLegenda.Name = "lbLegenda";
+            this.lbLegenda.Size = new System.Drawing.Size(0, 55);
+            this.lbLegenda.TabIndex = 1;
+            // 
+            // lbPontuacao
+            // 
+            this.lbPontuacao.AutoSize = true;
+            this.lbPontuacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPontuacao.Location = new System.Drawing.Point(13, 13);
+            this.lbPontuacao.Name = "lbPontuacao";
+            this.lbPontuacao.Size = new System.Drawing.Size(115, 24);
+            this.lbPontuacao.TabIndex = 2;
+            this.lbPontuacao.Text = "Pontuação:";
+            // 
+            // lbPonto
+            // 
+            this.lbPonto.AutoSize = true;
+            this.lbPonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPonto.Location = new System.Drawing.Point(124, 13);
+            this.lbPonto.Name = "lbPonto";
+            this.lbPonto.Size = new System.Drawing.Size(21, 24);
+            this.lbPonto.TabIndex = 3;
+            this.lbPonto.Text = "0";
+            // 
+            // picEscolha
+            // 
+            this.picEscolha.Location = new System.Drawing.Point(311, 106);
+            this.picEscolha.Name = "picEscolha";
+            this.picEscolha.Size = new System.Drawing.Size(153, 141);
+            this.picEscolha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEscolha.TabIndex = 4;
+            this.picEscolha.TabStop = false;
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(153, 167);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(505, 39);
+            this.lblStart.TabIndex = 5;
+            this.lblStart.Text = "Aperte qulquer tecla para iniciar";
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.picEscolha);
+            this.Controls.Add(this.lbPonto);
+            this.Controls.Add(this.lbPontuacao);
+            this.Controls.Add(this.lbLegenda);
+            this.Controls.Add(this.gpEscolhas);
+            this.Name = "frmGame";
+            this.Text = "JOKENPO";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmGame_KeyPress);
+            this.gpEscolhas.ResumeLayout(false);
+            this.gpEscolhas.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picEscolha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpEscolhas;
+        private System.Windows.Forms.RadioButton rbTesoura;
+        private System.Windows.Forms.RadioButton rbPapel;
+        private System.Windows.Forms.RadioButton rbDefault;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbLegenda;
+        private System.Windows.Forms.RadioButton rbPedra;
+        private System.Windows.Forms.Label lbPontuacao;
+        private System.Windows.Forms.Label lbPonto;
+        private System.Windows.Forms.PictureBox picEscolha;
+        private System.Windows.Forms.Label lblStart;
+    }
+}
+
---
+++ JOKENPO/GAME/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using RadioButton = System.Windows.Forms.RadioButton;
+
+namespace GAME
+{
+    public partial class frmGame : Form
+    {
+        string cpuEscolha;
+        int tempo;
+        int ponto;
+        RadioButton escolha;
+        bool iniciar;
+        public frmGame()
+        {
+            InitializeComponent();
+            NovoJogo();
+            lblStart.Visible = true;
+            gpEscolhas.Enabled = false;
+            iniciar=false;
+        }
+
+        private void NovoJogo()
+        {
+            this.BackColor = Color.White;
+            ponto = 0;
+            rbDefault.Checked = true;
+            picEscolha.Image = null;
+            tempo = 4;
+            lbPonto.Text = ponto.ToString();
+            timer1.Start();
+        }
+
+        private void Escolha()
+        {
+            Random random = new Random();
+            switch (random.Next(0, 3))
+            {
+                case 0:
+                    cpuEscolha = "Pedra";
+                    break;
+                case 1:
+                    cpuEscolha = "Papel";
+                    break;
+                case 2:
+                    cpuEscolha = "Tesoura";
+                    break;
+            }
+            picEscolha.Image = Image.FromFile($"{cpuEscolha}.png");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (iniciar)
+            {
+                tempo--;
+                lbLegenda.Visible = true;
+                lbLegenda.Text = tempo == 3 ? "JO" : tempo == 2 ? "KEN" : "PO!";
+
+                if (tempo == 0)
+                {
+                    timer1.Stop();
+                    Fim();
+                }
+            }
+
+        }
+
+        private void Fim()
+        {
+            Escolha();
+            escolha = gpEscolhas.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+
+            if (escolha.Text != "Default")
+            {
+                Embate();
+            }
+            else
+            {
+                GameOver();
+            }
+            Continuar();
+        }
+
+        private void Continuar()
+        {
+            this.BackColor = Color.White;
+            timer1.Start();
+            picEscolha.Image = null;
+            rbDefault.Checked = true;
+            tempo = 4;
+        }
+
+        private void Embate()
+        {
+            lbLegenda.Visible = false;
+
+            if (escolha.Text == "Pedra"&&cpuEscolha=="Papel"||
+                escolha.Text == "Papel"&&cpuEscolha=="Tesoura"||
+                escolha.Text == "Tesoura"&&cpuEscolha=="Pedra")
+            {
+                GameOver();
+            }else if (escolha.Text == cpuEscolha)
+            {
+                this.BackColor = Color.Gray;
+                MessageBox.Show($"Escolheram {escolha.Text}", "Empate");
+                Continuar();
+            }
+            else
+            {
+                this.BackColor = Color.Green;
+                Vitoria();
+            }
+        }
+
+        private void Vitoria()
+        {
+            MessageBox.Show($" {escolha.Text} ganha de {cpuEscolha}", "Vitoria");
+            ponto++;
+            lbPonto.Text=ponto.ToString();
+            Continuar() ;
+        }
+
+        private void GameOver()
+        {
+            this.BackColor = Color.Red;
+            lbLegenda.Visible = false;
+            if (escolha.Text=="Default")
+            {
+                MessageBox.Show("Não escolheu!", "Perdeu");
+            }
+            else
+            {
+                MessageBox.Show($" {escolha.Text} perde para {cpuEscolha} ", "Perdeu");
+            }
+            NovoJogo();
+        }
+
+        private void frmGame_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            iniciar = true;
+            lblStart.Visible = false;
+            gpEscolhas.Enabled = true;
+        }
+    }
+}
